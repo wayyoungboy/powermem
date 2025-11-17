ffae5b211431a5e263bb675edbc3d20fc80e9e85
渠磊 <xuyan.wxy@oceanbase.com>
2025-11-17 19:07:00 +0800

    feat(ci): add test frontend ci

--- docs/website/src/pages/index.tsx
+++ docs/website/src/pages/index.tsx
@@ -5,7 +5,7 @@ import Hero from '@site/src/components/Hero';
 import Features from '@site/src/components/Features';
 import ValueProps1 from '@site/src/components/ValueProps1';
 import QuickStart from '@site/src/components/QuickStart';
-import AnimatedBackground from '@site/src/components/AnimatedBackground';
+// import AnimatedBackground from '@site/src/components/AnimatedBackground';
 import MouseGlow from '@site/src/components/MouseGlow';
 import GridBackground from '@site/src/components/GridBackground';
 
@@ -17,7 +17,7 @@ export default function Home(): React.JSX.Element {
       description={siteConfig.tagline}>
       {/* Background Effects */}
       <GridBackground />
-      <AnimatedBackground />
+      {/* <AnimatedBackground /> */}
       <MouseGlow />
       <main>
         <Hero />
